6243549df6943e34a62853f783f4a6e95bc34f70
patu12 <mwangapatrick7@gmail.com>
2026-02-23 21:32:15 +0300

    Revert vite.config and remove GH Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', 'VITE_');
+    const env = loadEnv(mode, '.', '');
     return {
-      base: '/EDU-AI/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
